69a3ef7ddb7a2fc81e3d0b752add38f797daa625
sal281 <herryrev13245@gmail.com>
2024-08-31 14:06:03 -0600

    make new routes salman

---
+++ src/app/(home)/service-details/page.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+
+const Servicedetails = () => {
+  return (
+    <>
+    <div className="lg:pb-15 pb-10">
+        <div className="bg-warm lg:py-15 py-10">
+            <div className="container">
+                <div className="flex  md:flex-row flex-col justify-between items-center gap-10">
+                    <div className="">
+                        <h2 className="xl:text-[70px] lg:text-6xl md:text-5xl text-4xl font-bold leading-[117%]">Service Details</h2>
+                        <ul className="lg:pt-5 pt-3 flex items-center lg:gap-5 gap-2">
+                            <li><a href="#" className="lg:text-[28px] text-xl font-bold">Home</a></li>
+                            <li><i className="fa-solid fa-angle-right"></i></li>
+                            <li><a href="#" className="lg:text-[28px] text-xl font-bold">Service Details</a></li>
+                        </ul>
+                    </div>
+                    <div className="relative">
+                        <img src="assets/images/shapes/bread-cat.png" alt="cat-img" className="absolute bottom-5 -left-[30px] animate-up-down" />
+                        <img src="assets/images/shapes/bread-thumb.png" alt="thumb-img" className="sm:max-h-full max-h-60" />
+                        <img src="assets/images/shapes/bread-child.png" alt="child-img" className="absolute bottom-0 right-0 animate-left-right" />
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section className="lg:pt-15 pt-10">
+        <div className="container">
+            <div className="wow fadeInUp" data-wow-delay=".2s">
+                <img src="assets/images/services/service-details-1.png" alt="details" />
+            </div>
+            <div className="lg:pt-7.5 pt-5 wow fadeInUp" data-wow-delay=".3s">
+                <h4 className="lg:text-3xl text-2xl lg:leading-[148%] leading-[130%] font-bold  font-nunito">Discover the power of education Knowledge is the key</h4>
+                <p className="lg:pt-7 pt-4">There are many variations passages a This category focariatio of deign Redefining the an the  concept of living This category focuses on the design and construction of bu a arrangement furniture and decorative elements within them focariatio of deign There are many variations passages a This category focariatio of deign Redefining the an the  concept of living This category focuses on the design and construction of bu a arrangement furniture</p>
+                <div className="lg:pt-7.5 pt-5 flex flex-col lg:flex-row justify-between lg:gap-7.5 lg:gap-y-[22px] gap-4">
+                    <div className="flex flex-col lg:gap-y-[22px] gap-4">
+                        <p className="flex items-center gap-[15px]"><i className="fa-solid fa-angle-right text-secondary-foreground"></i> Education is the key to success. It empowers individuals with knowledge</p>
+                        <p className="flex items-center gap-[15px]"><i className="fa-solid fa-angle-right text-secondary-foreground"></i> Education opens doors to opportunities, fosters critical thinking, and promote</p>
+                    </div>
+                    <div className="flex flex-col lg:gap-y-[22px] gap-4">
+                        <p className="flex items-center gap-[15px]"><i className="fa-solid fa-angle-right text-secondary-foreground"></i> Education is the key to success. It empowers individuals with knowledge</p>
+                        <p className="flex items-center gap-[15px]"><i className="fa-solid fa-angle-right text-secondary-foreground"></i> Education opens doors to opportunities, fosters critical thinking, and promote</p>
+                    </div>
+                </div>
+                <div className="mt-10 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-7.5">
+                    <div>
+                        <img src="assets/images/services/service-details-2.png" alt="img-1" className="w-full" />
+                    </div>
+                    <div>
+                        <img src="assets/images/services/service-details-3.png" alt="img-1" className="w-full" />
+                    </div>
+                    <div>
+                        <img src="assets/images/services/service-details-4.png" alt="img-1" className="w-full" />
+                    </div>
+                </div>
+                <div className="lg:pt-[70px] pt-10 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-7.5">
+                  
+                    <div className="bg-warm rounded-[10px] lg:p-7.5 p-4 ">
+                        <div className="bg-background rounded-[10px] p-3.5 lg:max-w-[88px] max-w-[70px] lg:max-h-[88px] max-h-[70px]">
+                            <i className="icon-car-toy lg:text-6xl text-[40px] text-green-foreground"></i>
+                        </div>
+                        <div className="lg:mt-5 pt-3">
+                            <h4 className="text-2xl font-semibold leading-[140%]">Online className</h4>
+                            <p className="lg:pt-[18px] pt-3">Lorem ipsum dolor sit amet consecte adipiscing elit platea  the ,osr of a proin mollis</p>
+                        </div>
+                    </div>
+                    
+                    <div className="bg-warm rounded-[10px] lg:p-7.5 p-4 ">
+                        <div className="bg-background rounded-[10px] p-3.5 lg:max-w-[88px] max-w-[70px] lg:max-h-[88px] max-h-[70px]">
+                            <i className="icon-teddy-bear lg:text-6xl text-[40px] text-green-foreground"></i>
+                        </div>
+                        <div className="lg:mt-5 pt-3">
+                            <h4 className="text-2xl font-semibold leading-[140%]">Online className</h4>
+                            <p className="lg:pt-[18px] pt-3">Lorem ipsum dolor sit amet consecte adipiscing elit platea  the ,osr of a proin mollis</p>
+                        </div>
+                    </div>
+                    
+                    <div className="bg-warm rounded-[10px] lg:p-7.5 p-4 ">
+                        <div className="bg-background rounded-[10px] p-3.5 lg:max-w-[88px] max-w-[70px] lg:max-h-[88px] max-h-[70px]">
+                            <i className="icon-car-toy lg:text-6xl text-[40px] text-green-foreground"></i>
+                        </div>
+                        <div className="lg:mt-5 pt-3">
+                            <h4 className="text-2xl font-semibold leading-[140%]">Online className</h4>
+                            <p className="lg:pt-[18px] pt-3">Lorem ipsum dolor sit amet consecte adipiscing elit platea  the ,osr of a proin mollis</p>
+                        </div>
+                    </div>
+              
+              
+                </div>
+            </div>
+            <div className="pt-10">
+                <div className="max-w-[790px] wow fadeInUp" data-wow-delay=".4s">
+                    <h4 className="text-2xl font-semibold leading-[140%]">Service all details</h4>
+                    <p className="mt-[18px]">There are many variations passages a This category focariatio of deign Redefining the an the  concept of living This category focuses on the design and construction of bu a arrangement furniture and decorative elements within them focariatio of deign</p>
+                </div>
+                <div className="grid lg:grid-cols-2 grid-cols-1 items-center gap-7.5 lg:pt-7.5 pt-5 wow fadeInUp" data-wow-delay=".5s">
+                    <ul className="flex lg:gap-y-7.5 gap-y-5 flex-col">
+                        <li className="flex items-center gap-[15px] text-[#686868]"><i className="fa-solid fa-angles-right text-secondary-foreground"></i> <span>It plays a vital role in shaping the future generation and promoting</span> </li>
+                        <li className="flex items-center gap-[15px] text-[#686868]"><i className="fa-solid fa-angles-right text-secondary-foreground"></i> <span>Education encompasses formal learning in schools, colleges, and universities</span> </li>
+                        <li className="flex items-center gap-[15px] text-[#686868]"><i className="fa-solid fa-angles-right text-secondary-foreground"></i> <span>Education opens doors to opportunities, fosters critical thinking, and promote</span> </li>
+                        <li className="flex items-center gap-[15px] text-[#686868]"><i className="fa-solid fa-angles-right text-secondary-foreground"></i> <span>Man encompasses formal learning in schools, colleges, and universities</span> </li>
+                    </ul>
+                    <div>
+                        <img src="assets/images/services/service-details-5.png" alt="service-details-5" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section className="lg:pt-15 pt-10 ">
+        <div className="bg-warm py-12.5 relative z-[1]">
+            <div className="container">
+                <div className="flex md:flex-row flex-col justify-between items-center gap-10">
+                    <div className="lg:max-w-[573px] max-w-[400px]">
+                        <p className="text-muted-foreground font-bubblegum-sans text-[19px] wow fadeInUp" >Stay With Us</p>
+                        <h2 className="font-bold lg:text-[32px] md:text-[28px] text-2xl lg:leading-[130%] md:leading-[120%] leading-[110%] mt-2.5 max-w-[410px] wow fadeInUp" data-wow-delay=".3s">The path to success starts with education</h2>
+                        <p className="mt-5 wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet consectetur. Amet lectus mi ultricies dictum facilisis sem. Imperdiet massa turpis sit proin metus volutpat.</p>
+                        <div className="mt-9">
+                            <a href="#" className="btn-rounded-full">Read More</a>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <img src="assets/images/newsletter/stay-thumb.png" alt="tree-img" />
+                    </div>
+                </div>
+            </div>
+            <div className="absolute left-0 bottom-0 z-[-1]">
+                <img src="assets/images/newsletter/stay-shape.png" alt="stay-shape" />
+            </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default Servicedetails;
---
+++ src/app/(home)/services/page.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <>
+     <div className="lg:pb-15 pb-10">
+        <div className="bg-warm lg:py-15 py-10">
+            <div className="container">
+                <div className="flex  md:flex-row flex-col justify-between items-center gap-10">
+                    <div className="">
+                        <h2 className="xl:text-[70px] lg:text-6xl md:text-5xl text-4xl font-bold leading-[117%]">Services</h2>
+                        <ul className="lg:pt-5 pt-3 flex items-center lg:gap-5 gap-2">
+                            <li><a href="#" className="lg:text-[28px] text-xl font-bold">Home</a></li>
+                            <li><i className="fa-solid fa-angle-right"></i></li>
+                            <li><a href="#" className="lg:text-[28px] text-xl font-bold">Services</a></li>
+                        </ul>
+                    </div>
+                    <div className="relative">
+                        <img src="assets/images/shapes/bread-cat.png" alt="cat-img" className="absolute bottom-5 -left-[30px] animate-up-down" />
+                        <img src="assets/images/shapes/bread-thumb.png" alt="thumb-img" className="sm:max-h-full max-h-60" />
+                        <img src="assets/images/shapes/bread-child.png" alt="child-img" className="absolute bottom-0 right-0 animate-left-right" />
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div className="lg:pb-15 pb-10">
+        <div className="container">
+            <div className="lg:pl-11">
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-y-7.5 lg:gap-x-[74px] gap-x-5 lg:pt-15 pt-10">
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".3s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                            <i className="icon-car-toy md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Online className</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".4s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                            <i className="icon-toys md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Formal Tuition</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".5s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                           <i className="icon-feeder md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Preschool</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".6s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                            <i className="icon-book md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Special Tuition</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".7s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                            <i className="icon-mat md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Edu Connect</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="relative rounded-[10px] border-2 bg-background border-[#F2F2F2] lg:p-10 p-4 transition-all duration-500 hover:shadow-3xl hover:border-transparent group/card wow fadeInUp" data-wow-delay=".8s">
+                        <div className="md:max-w-[88px] max-w-[70px] w-full max-h-[88px] flex justify-center items-center rounded-[10px] border border-[#F2F2F2] bg-background sm:p-[14px] p-2.5 static lg:absolute -left-11 top-1/2 lg:-translate-y-1/2 transition-all duration-500 text-green-foreground group-hover/card:bg-green group-hover/card:text-cream-foreground">
+                            <i className="icon-baby-body md:text-6xl text-[40px]"></i>
+                        </div>
+                        <div className="lg:pl-11 mt-4 lg:mt-0">
+                            <h4>
+                                <a href="service-details.html" className="font-semibold lg:text-2xl text-xl group-hover/card:text-green-foreground transition-all duration-500">Knowledge Hub</a>
+                            </h4>
+                            <p className="lg:mt-4 mt-3">Lorem ipsum dolor sit amet consectetur adipiscing elit platea the ,osr of a proin mollis pretium facilisi in, ligula volutpat </p>
+                            <a href="service-details.html" className="inline-flex items-center gap-2.5 lg:mt-7.5 mt-4 group/btn">
+                                <span className="group-hover/btn:text-green-foreground transition-all duration-500">Read More</span> 
+                                <span className="group-hover/btn:ml-1 group-hover/btn:text-green-foreground transition-all duration-500"> <i className="fa-solid fa-arrow-right"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                     
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section className="lg:pt-15 pt-10 ">
+        <div className="bg-warm py-12.5 relative z-[1]">
+            <div className="container">
+                <div className="flex md:flex-row flex-col justify-between items-center gap-10">
+                    <div className="lg:max-w-[573px] max-w-[400px]">
+                        <p className="text-muted-foreground font-bubblegum-sans text-[19px] wow fadeInUp" >Stay With Us</p>
+                        <h2 className="font-bold lg:text-[32px] md:text-[28px] text-2xl lg:leading-[130%] md:leading-[120%] leading-[110%] mt-2.5 max-w-[410px] wow fadeInUp" data-wow-delay=".3s">The path to success starts with education</h2>
+                        <p className="mt-5 wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet consectetur. Amet lectus mi ultricies dictum facilisis sem. Imperdiet massa turpis sit proin metus volutpat.</p>
+                        <div className="mt-9">
+                            <a href="#" className="btn-rounded-full">Read More</a>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <img src="assets/images/newsletter/stay-thumb.png" alt="tree-img" />
+                    </div>
+                </div>
+            </div>
+            <div className="absolute left-0 bottom-0 z-[-1]">
+                <img src="assets/images/newsletter/stay-shape.png" alt="stay-shape" />
+            </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default Services;
