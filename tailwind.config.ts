f247621d07cb1c107d40e0fa9b4355469dcdf504
Munazzar Khan <gex.18@hotmail.com>
2024-09-03 16:43:28 +0500

    tailwind config changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import { nextui } from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  important: true,
   content: [
     "./src/pages/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
