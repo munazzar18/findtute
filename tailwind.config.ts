9eb58875f76cba892c81b5a9a68405bf77b61edf
Munazzar Khan <gex.18@hotmail.com>
2024-09-03 11:32:33 +0500

    added important to  tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import { nextui } from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  important: true,
   content: [
     "./src/pages/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/components/(button|input|navbar|progress|ripple|spinner).js",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
